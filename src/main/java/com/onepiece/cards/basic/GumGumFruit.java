64f26aa2b781d503a6acc863d5c548ba92aef0ae
ZTGAME\v-zhaoqian <v-zhaoqian@ztgame.com>
2025-04-25 18:08:23 +0800

    《《《《《《新改动》》》》》

--- src/main/java/com/onepiece/cards/basic/GumGumFruit.java
+++ src/main/java/com/onepiece/cards/basic/GumGumFruit.java
@@ -36,11 +36,7 @@ import static com.onepiece.characters.LuffyChar.PlayerColorEnum.Luffy_RED;
 //@AutoAdd.Ignore
 public class GumGumFruit extends CustomCard {
 
-    public static final String ID = ModHelper.makePath("GumGumFruit");
-    private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
-    private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
-    private static final String IMG_PATH = "LuffyModRes/img/cards/GumGumFruit.png";//todo:改成橡胶果实icon
-    private static final int COST = 0;
+   
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION; // 读取本地化的描述
     private static final CardType TYPE = CardType.POWER;
     private static final CardColor COLOR = Luffy_RED;
